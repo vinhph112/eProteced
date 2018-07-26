de35d018cfdf3961717eb07c2dc2fb8d890e6a3e
Vinh Pham <technical.vinh@smartec.com.vn>
2018-07-26 16:44:35 +0700

    update file app.js

--- App.js
+++ App.js
@@ -1,117 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
   Platform,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  Image
 } from 'react-native';
+import Main from './components/sources/Main';
 
-import Pulse from 'react-native-pulse';
-import * as Animatable from 'react-native-animatable';
-//import image
-import img_ble from './components/images/ble_icon.png'
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 
 export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      isPressedScan: false
-    }
-  }
-  startScan() {
-    this.setState({isPressed: ! this.state.isPressed})
-  }
-  showPulse() {
-    if (this.state.isPressed) {
-      return (
-        <Pulse
-          color='#4AB6FF'
-          numPulses={5}
-          diameter={400}
-          speed={20}
-          duration={2000}
-        />
-      );
-    }
-    else {
-      return null;
-    }
-  }
+
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={styles.container}>
-          <View>
-            {this.showPulse()}
-            <TouchableOpacity
-              style = {styles.btnBleStyle}
-              onPress = {this.startScan.bind(this)}>
-              <Image
-                  style={{width: 50, height: 50}}
-                  source = {img_ble}
-              />
-            </TouchableOpacity>
-          </View>
-          <Animatable.Text
-            animation="pulse"
-            iterationCount= "infinite"
-            direction="alternate"
-            style={ styles.txtTapStyle}
-          >
-          {this.state.isPressed ? '' : 'TAP ICON TO SCAN' }
-          </Animatable.Text>
-      </View>
-      <Animatable.Text
-        animation="pulse"
-        iterationCount= "infinite"
-        direction="alternate-reverse"
-        style={styles.txtScaningStyle}
-      >
-      {this.state.isPressed ? 'Scanning for devices' : 'Scan done' }
-      </Animatable.Text>
-    </View>
+      <Provider store = { store }>
+          <Main />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  btnBleStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4AB6FF',
-    width: 80,
-    height: 80,
-    borderRadius: 80,
-    borderWidth: 1,
-    borderColor: 'transparent'
-  },
-  txtTapStyle: {
-    textAlign: 'center',
-    margin: 10
-  },
-  txtScaningStyle: {
-    alignItems: 'flex-end',
-    textAlign: 'center',
-    marginBottom: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+//defaultState
+const defaultState = {
+    arrBLEs: [
+    ],
+  isConnected: false,
+  tabCurrent: 'home',
+  isTaptoScan: false,
+  name: '',
+  mac: '',
+  isShowDialogControl: false,
+};
+const reducer = ( state = defaultState, action) => {
+  switch (action.type) {
+    case 'HOME_TAB':
+      return {...state, tabCurrent: 'home', isTaptoScan: false};
+    case 'DEVICE_TAB':
+      return {...state, tabCurrent: 'devices'};
+    case 'CONTROL_TAB':
+      return {...state, tabCurrent: 'control'};
+    case 'SETTING_TAB':
+      return {...state, tabCurrent: 'setting'};
+    case 'PRESS_SCAN_TRUE':
+      return {...state, isTaptoScan: true, arrBLEs: []};
+    case 'PRESS_SCAN_FALSE':
+      return {...state, isTaptoScan: false};
+    case 'CONNECTED_BLE':
+      return {
+        ...state,
+        isConnected: true,
+        name: action.name,
+        mac: action.mac
+      }
+    case 'DISCONNECTED_BLE':
+      return {
+        ...state,
+        isConnected: false,
+        name: '',
+        mac: ''
+      };
+    case 'SHOW_DIALOG_CONTROL':
+      return {...state, isShowDialogControl: true};
+    case 'CLOSE_DIALOG_CONTROL':
+      return {...state, isShowDialogControl: false};
+    case 'ADD_BLE_TO_ARR':
+      return {
+        ...state,
+        arrBLEs: state.arrBLEs.concat({
+          id: state.arrBLEs.length + 1,
+          mac: action.mac,
+          name: action.name,
+        })
+      };
+    default:
+      break;
+  }
+  return state;
+};
+//
+const store = createStore(reducer);
